53f44338fde441e49e77efa088191c03341fc95c
ianarnold <arnoldian2003@gmail.com>
2022-09-16 10:50:44 -0300

    segundo commit

--- Envia/Form1.Designer.cs
+++ Envia/Form1.Designer.cs
@@ -33,13 +33,20 @@
             this.rchTxtResultado = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.txtDescriptografia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbSimetrica = new System.Windows.Forms.RadioButton();
+            this.rdbAssimetrica = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtChave = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnCriptografar
             // 
-            this.btnCriptografar.Location = new System.Drawing.Point(13, 41);
+            this.btnCriptografar.Location = new System.Drawing.Point(19, 155);
             this.btnCriptografar.Name = "btnCriptografar";
-            this.btnCriptografar.Size = new System.Drawing.Size(201, 115);
+            this.btnCriptografar.Size = new System.Drawing.Size(333, 32);
             this.btnCriptografar.TabIndex = 0;
             this.btnCriptografar.Text = "Criptografar";
             this.btnCriptografar.UseVisualStyleBackColor = true;
@@ -47,7 +54,7 @@
             // 
             // txtCriptografia
             // 
-            this.txtCriptografia.Location = new System.Drawing.Point(13, 15);
+            this.txtCriptografia.Location = new System.Drawing.Point(151, 90);
             this.txtCriptografia.Name = "txtCriptografia";
             this.txtCriptografia.Size = new System.Drawing.Size(201, 20);
             this.txtCriptografia.TabIndex = 1;
@@ -55,17 +62,17 @@
             // rchTxtResultado
             // 
             this.rchTxtResultado.Enabled = false;
-            this.rchTxtResultado.Location = new System.Drawing.Point(220, 15);
+            this.rchTxtResultado.Location = new System.Drawing.Point(20, 202);
             this.rchTxtResultado.Name = "rchTxtResultado";
-            this.rchTxtResultado.Size = new System.Drawing.Size(426, 141);
+            this.rchTxtResultado.Size = new System.Drawing.Size(332, 201);
             this.rchTxtResultado.TabIndex = 3;
             this.rchTxtResultado.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 208);
+            this.button1.Location = new System.Drawing.Point(19, 424);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(633, 32);
+            this.button1.Size = new System.Drawing.Size(333, 32);
             this.button1.TabIndex = 4;
             this.button1.Text = "Descriptografar";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,16 +81,90 @@
             // txtDescriptografia
             // 
             this.txtDescriptografia.Enabled = false;
-            this.txtDescriptografia.Location = new System.Drawing.Point(13, 260);
+            this.txtDescriptografia.Location = new System.Drawing.Point(139, 475);
             this.txtDescriptografia.Name = "txtDescriptografia";
-            this.txtDescriptografia.Size = new System.Drawing.Size(633, 20);
+            this.txtDescriptografia.Size = new System.Drawing.Size(213, 20);
             this.txtDescriptografia.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Texto a ser criptografado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 478);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Texto descriptografado:";
+            // 
+            // rdbSimetrica
+            // 
+            this.rdbSimetrica.AutoSize = true;
+            this.rdbSimetrica.Location = new System.Drawing.Point(151, 32);
+            this.rdbSimetrica.Name = "rdbSimetrica";
+            this.rdbSimetrica.Size = new System.Drawing.Size(68, 17);
+            this.rdbSimetrica.TabIndex = 8;
+            this.rdbSimetrica.TabStop = true;
+            this.rdbSimetrica.Text = "Simétrica";
+            this.rdbSimetrica.UseVisualStyleBackColor = true;
+            this.rdbSimetrica.CheckedChanged += new System.EventHandler(this.rdbSimetrica_CheckedChanged);
+            // 
+            // rdbAssimetrica
+            // 
+            this.rdbAssimetrica.AutoSize = true;
+            this.rdbAssimetrica.Location = new System.Drawing.Point(274, 32);
+            this.rdbAssimetrica.Name = "rdbAssimetrica";
+            this.rdbAssimetrica.Size = new System.Drawing.Size(78, 17);
+            this.rdbAssimetrica.TabIndex = 9;
+            this.rdbAssimetrica.TabStop = true;
+            this.rdbAssimetrica.Text = "Assimétrica";
+            this.rdbAssimetrica.UseVisualStyleBackColor = true;
+            this.rdbAssimetrica.CheckedChanged += new System.EventHandler(this.rdbAssimetrica_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tipo de chave:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Chave de criptografia:";
+            // 
+            // txtChave
+            // 
+            this.txtChave.Location = new System.Drawing.Point(151, 120);
+            this.txtChave.Name = "txtChave";
+            this.txtChave.Size = new System.Drawing.Size(201, 20);
+            this.txtChave.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 413);
+            this.ClientSize = new System.Drawing.Size(375, 520);
+            this.Controls.Add(this.txtChave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rdbAssimetrica);
+            this.Controls.Add(this.rdbSimetrica);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtDescriptografia);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rchTxtResultado);
@@ -102,6 +183,13 @@
         private System.Windows.Forms.RichTextBox rchTxtResultado;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtDescriptografia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdbSimetrica;
+        private System.Windows.Forms.RadioButton rdbAssimetrica;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtChave;
     }
 }
 
--- Envia/Form1.cs
+++ Envia/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -12,19 +14,38 @@ namespace Envia
 {
     public partial class Form1 : Form
     {
-        ChaveAssimetrica key;
+        ChaveAssimetrica keyAss;
+        ChaveSimetrica keySi;
         public Form1()
         {
             InitializeComponent();
-            key = new ChaveAssimetrica();
+            keyAss = new ChaveAssimetrica();
+            keySi = new ChaveSimetrica();
+            txtChave.Visible = false;
+            label4.Visible = false;
         }
 
         private void btnCriptografar_Click(object sender, EventArgs e)
         {
-            string frase, fraseCripto;
-            frase = txtCriptografia.Text;
-            fraseCripto = key.encrypt(frase);
-            rchTxtResultado.Text = fraseCripto;
+            if (!rdbSimetrica.Checked && !rdbAssimetrica.Checked)
+            {
+                MessageBox.Show("Selecione um tipo de chave para a criptografia.");
+            } 
+            else if (rdbSimetrica.Checked)
+            {
+                txtChave.Visible = true;
+                label4.Visible = true;
+            } 
+            else if (rdbAssimetrica.Checked)
+            {
+                txtChave.Visible = false;
+                label4.Visible = false;
+                string frase, fraseCripto;
+                frase = txtCriptografia.Text;
+                fraseCripto = keyAss.encrypt(frase);
+                rchTxtResultado.Text = fraseCripto;
+            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,10 +57,37 @@ namespace Envia
             {
                 string fraseDescrip, fraseCripto;
                 fraseCripto = rchTxtResultado.Text;
-                fraseDescrip = key.decrypt(fraseCripto);
+                fraseDescrip = keyAss.decrypt(fraseCripto);
                 txtDescriptografia.Text = fraseDescrip;
             }
             
         }
+
+        private void rdbSimetrica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbSimetrica.Checked)
+            {
+                txtChave.Visible = true;
+                label4.Visible = true;
+            }
+            else if (rdbAssimetrica.Checked)
+            {
+                txtChave.Visible = false;
+                label4.Visible = false;
+            }
+        }
+        private void rdbAssimetrica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbSimetrica.Checked)
+            {
+                txtChave.Visible = true;
+                label4.Visible = true;
+            }
+            else if (rdbAssimetrica.Checked)
+            {
+                txtChave.Visible = false;
+                label4.Visible = false;
+            }
+        }
+    }   
     }
-}
